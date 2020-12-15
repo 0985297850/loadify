0e8edf882e8a08ca2df750cf757c4fbec91e2578
Phong dzai <dinhphong22082000@gmail.com>
2020-12-15 16:12:14 +0700

    hello

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var button =document.querySelector('.header__menu-bars');
+var listMenu =document.querySelector('.list-menu');
+button.addEventListener('click',function(){
+    listMenu.classList.toggle('show')
+    
+})
